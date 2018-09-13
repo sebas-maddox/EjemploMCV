99dda789ad20ac6cbce74c7bcf815fb369226b95
sebas-maddox <42845290+sebas-maddox@users.noreply.github.com>
2018-09-12 22:25:58 -0500

    Actualizacion
    relaciones y cambio a singular

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,13 +1,13 @@
-DROP TABLE Asociaciones
-DROP TABLE Trabajos
-DROP TABLE Mensajes
-DROP TABLE Eventos
-DROP TABLE Comentarios
-DROP TABLE Noticias
-DROP TABLE Categorias
-DROP TABLE Usuarios
+DROP TABLE Asociacion
+DROP TABLE Trabajo
+DROP TABLE Mensaje
+DROP TABLE Evento
+DROP TABLE Comentario
+DROP TABLE Noticia
+DROP TABLE Categoria
+DROP TABLE Usuario
 
-CREATE TABLE Usuarios(
+CREATE TABLE Usuario(
 	LoginID nvarchar(100) PRIMARY KEY,
 	Contra nvarchar(100) NOT NULL,
 	Nombre nvarchar(200),
@@ -16,32 +16,36 @@ CREATE TABLE Usuarios(
 	Administrador BIT NOT NULL DEFAULT 0
 	)
 
-CREATE TABLE Categorias(
+CREATE TABLE Categoria(
 	IdCategoria bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Nombre nvarchar(100)
-	)
+)
+	
 
-CREATE TABLE Noticias(
+CREATE TABLE Noticia(
 	IdNot bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Titulo nvarchar(100),
 	Cuerpo text,
-	LoginID nvarchar(100),
+	LoginID bigint NOT NULL,
 	IdCategoria bigint,
-	FechaPublicacion datetime NOT NULL DEFAULT getdate()
+	FechaPublicacion datetime NOT NULL DEFAULT getdate(),
+	FOREIGN KEY (IdCategoria) REFERENCES Categoria(IdCategoria)
 	)
 
 
-CREATE TABLE Comentarios (
+CREATE TABLE Comentario (
 	IdComentario bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Comentario text,
 	LoginID nvarchar(100),
 	IdNot bigint,
 	Estado BIT NOT NULL DEFAULT 0,
-	FechaCreacion datetime NOT NULL DEFAULT getdate()
+	FechaCreacion datetime NOT NULL DEFAULT getdate(),
+	FOREIGN KEY (LoginID) REFERENCES Usuario(LoginID)
+
 	)
 
 
-CREATE TABLE Eventos(
+CREATE TABLE Evento(
 	IdEvento bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Nombre nvarchar(100),
 	Fecha date,
@@ -49,24 +53,27 @@ CREATE TABLE Eventos(
 	)
 
 
-CREATE TABLE Mensajes(
+CREATE TABLE Mensaje(
 	IdMensaje bigint NOT NULL IDENTITY(1,1) PRIMARY  KEY,
 	IdUsuarioEnv nvarchar(100),
-	IdUsuarioRec nvarchar(100),
-	Mensaje text
+	IdUsuarioRec nvarchar(100)
+	Mensaje text,
+	FOREIGN KEY (IdUsuarioRec) REFERENCES Usuario(LoginID),
+	FOREIGN KEY (IdUsuarioEnv) REFERENCES Usuario(LoginID)
 	)
 
-CREATE TABLE Trabajos(
+CREATE TABLE Trabajo(
 	IdTrabajo bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	LoginID nvarchar(100),
 	Titulo nvarchar(100),
 	Lugar nvarchar(100),
 	Descripcion text,
 	FechaPublicacion datetime NOT NULL DEFAULT getdate(),
-	Tipo nvarchar(100)
+	Tipo nvarchar(100),
+	FOREIGN KEY (LoginID) REFERENCES Usuario(LoginID)
 	)
 
-CREATE TABLE Asociaciones(
+CREATE TABLE Asociacion(
 	IdAsociacion bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Nombre nvarchar(100),
 	Ubicacion nvarchar(200),
